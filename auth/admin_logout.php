83b6e659e89cb05192fb1506507f83794d83d0e9
ZenS <ammarsyrf78@gmail.com>
2025-10-06 23:39:10 +0700

    logout admin fix

--- auth/admin_logout.php
+++ auth/admin_logout.php
@@ -2,20 +2,8 @@
 // File: admin_logout.php
 // Logout untuk Admin dan Superadmin
 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-// Hapus semua session admin
-if (isset($_SESSION['admin_logged_in'])) {
-    unset($_SESSION['admin_logged_in']);
-    unset($_SESSION['admin_id']);
-    unset($_SESSION['admin_username']);
-    unset($_SESSION['admin_name']);
-    unset($_SESSION['admin_email']);
-    unset($_SESSION['admin_role']);
-}
-
-// Redirect ke halaman login dengan pesan sukses
-header('Location: ../admin/index.php');
-exit();
+session_start();
+session_unset();
+session_destroy();
+header("Location: admin_login.php");
+exit;
